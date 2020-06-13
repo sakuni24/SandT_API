4019c503362654d67bb8ecc95e73e77d8f66220e
piyumaldk <piyumaldk@gmail.com>
2020-06-14 02:12:29 +0530

    Request Find By Event ID

--- sandtgroup/src/main/java/com/api/repository/EventRequestRepository.java
+++ sandtgroup/src/main/java/com/api/repository/EventRequestRepository.java
@@ -1,9 +1,11 @@
 package com.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.api.model.EventRequest;
 
 public interface EventRequestRepository extends MongoRepository<EventRequest, String>{
-
+	List<EventRequest> findByEventId(String eventId);
 }
--- sandtgroup/src/main/java/com/api/resource/EventRequestController.java
+++ sandtgroup/src/main/java/com/api/resource/EventRequestController.java
@@ -4,11 +4,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+//import org.springframework.data.mongodb.core.query.Criteria;
+//import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.model.EventRequest;
@@ -35,6 +41,11 @@ public class EventRequestController {
 	@GetMapping("/getEventRequests")
 	public List<EventRequest> getEventRequests(){
 		return repository.findAll();
+	}
+	
+	@GetMapping("/getEventRequestsByEventId/{id}")
+	public List<EventRequest> getEventRequestByEventId(@PathVariable String id){
+		return repository.findByEventId(id);
 	}
 	
 	@GetMapping("/getEventRequests/{id}")
