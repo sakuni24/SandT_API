4340e28f82d2e778e0843120fff7b6a8c896441e
piyumaldk <piyumaldk@gmail.com>
2020-07-05 15:35:48 +0530

    Cors allow

--- sandtgroup/src/main/java/com/api/SandtgroupApplication.java
+++ sandtgroup/src/main/java/com/api/SandtgroupApplication.java
@@ -2,10 +2,13 @@ package com.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-public class SandtgroupApplication {
+@CrossOrigin(origins="http://localhost:3000")
 
+public class SandtgroupApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SandtgroupApplication.class, args);
 		//main function - run this as a java application 
